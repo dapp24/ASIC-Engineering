e5e0478e8b16908d96a725c895d5df1c85030309
dapp24 <daren.appanah@gmail.com>
2016-04-08 11:46:29 +0100

    Main test program
    most recent

---
+++ logread-script.py
@@ -0,0 +1,81 @@
+import os
+import csv
+from shutil import move
+from os import remove, close
+
+###################################
+#   Open RC script and change effort 
+###################################
+
+with open("scripts/simd_fpu.rc",'r+') as rcFile:
+	with open("scripts/temp.rc", 'w') as outrcFile:
+		print "simd_fpu.rc open, r+ mode"
+		for line in rcFile:
+			if line.startswith("synthesize"):
+				print "Line found"
+				#line = line[30:33]
+				#print l
+				line =line.replace("low", "high")
+				print "low replaced"
+				#print l
+			outrcFile.write(line)
+###################################
+#   Replace RC file with modified script 
+###################################
+remove('scripts/simd_fpu.rc')
+move(('scripts/temp.rc'), ('scripts/simd_fpu.rc'))	
+print "rc file saved"			
+
+
+###################################
+#  Execute scripts 
+###################################
+#os.system("./scripts/compile.sh~")
+#os.system("rc -logfile 10 -f scripts/simd_fpu.rc")
+#os.system("ecnounter -64 -log denc.log -init scripts/simd_fpu.enc")
+
+###################################
+#   Read RC log and output results
+###################################
+ 
+with open("dlog34",'r') as logFile:
+    print "file open, R mode"
+    for line in logFile:
+        	if line.startswith(" Slack"):
+          	 print "Slack :"
+                 l = logFile.next()
+		 l = logFile.next()
+		 l = l[2: l.find("p")]
+           	 print l;            
+
+
+###################################
+#   Read Encounter log and output results
+###################################
+ 
+with open("denc.log",'r') as logFile:
+    print "file open, R mode"
+    for line in logFile:
+        	if line.startswith("          timeDesign Summary"):
+		 n = 0;
+		 while n !=  6:
+		  l = logFile.next()
+	          if line.startswith(""):
+		   print "Slack"
+		   n = n+1;
+		  #while n == 0:
+          	   print "Slack"
+               		
+		   l = l[2: l.find("8")]
+           	   print l;         
+	           
+###################################
+#   Print results to CSV file
+###################################   
+f = open("scripts/enc_results.csv", 'w')
+RESULT = ['apple','cherry','orange','pineapple','strawberry']	
+c = csv.writer(f, dialect='excel')
+
+c.writerow(RESULT)
+
+
